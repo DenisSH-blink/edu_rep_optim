6a55b6ce0752f9865bc88db63ef35765e6bc66b7
Denis <SchwarzerKlok@yandex.ru>
2025-09-09 00:12:28 +0400

    Continue_1

--- service/services/views.py
+++ service/services/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from django.db.models import Prefetch, F
+from django.db.models import Prefetch, F, Sum
 from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializers
@@ -18,7 +18,9 @@ class SubscriptionView(ReadOnlyModelViewSet):
     serializer_class = SubscriptionSerializers
     
     def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
         response_data = {'result':response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
         response.data = response_data
         return response
